c24ff5c8e4bc8457dd3ffbb112201bef47dd9263
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-21 15:46:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\temp\K1");
+            Directory.CreateDirectory(@"C:\temp\K2");
+            Console.WriteLine("Папки были успешно созданы!");
+        }
+    }
+}
